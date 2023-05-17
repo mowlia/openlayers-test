2b4e1817ccb9761e630ed46bcb4c9ff23a19a4be
Mohammad Owlia <mohammad@joinsherpa.com>
2023-05-17 17:38:51 -0400

    new files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import GeoJSON from "ol/format/GeoJSON";
 import Map from "ol/Map";
-import VectorLayer from "ol/layer/Vector";
+import VectorLayer from "ol/layer/VectorImage";
 import VectorSource from "ol/source/Vector";
 import View from "ol/View";
 import { Fill, Stroke, Style, Text } from "ol/style.js";
@@ -9,11 +9,32 @@ import { DEVICE_PIXEL_RATIO } from "ol/has.js";
 // Gradient and pattern are in canvas pixel space, so we adjust for the
 // renderer's pixel ratio
 const pixelRatio = DEVICE_PIXEL_RATIO;
-const canvas = document.createElement("canvas");
-const context = canvas.getContext("2d");
-const gradient = context.createLinearGradient(0, 0, 1024 * pixelRatio, 0);
-gradient.addColorStop(0, "red");
-gradient.addColorStop(1, "purple");
+//const canvas = document.createElement("canvas");
+//const context = canvas.getContext("2d");
+//const gradient = context.createLinearGradient(0, 0, 1024 * pixelRatio, 0);
+//gradient.addColorStop(0, "red");
+//gradient.addColorStop(1, "purple");
+
+var styleOpen = new Style({
+  /*text: new Text({
+    text: "123"//feature.get("name"),
+  }),*/
+  stroke: new Stroke({
+    color: "red",
+    width: 2,
+  }),
+  fill: new Fill({
+    color: "red",
+    // color: gradient,
+  }),
+});
+
+var styleClose = new Style({
+  stroke: new Stroke({
+    color: "#3399CC",
+    width: 1.25,
+  }),
+});
 
 
 new Map({
@@ -22,29 +43,14 @@ new Map({
     new VectorLayer({
       source: new VectorSource({
         format: new GeoJSON(),
-        url: "./worldLow.json",
+        url: "./xs.json",
       }),
       style: function (feature, res) {
-        if (feature.get("name") == "Russia") {
-          return new Style({
-            text: new Text({
-              text: feature.get("name"),
-            }),
-            stroke: new Stroke({
-              color: "red",
-              width: 2,
-            }),
-            fill: new Fill({
-              color: gradient,
-            }),
-          });
+        if (feature.get("name") == "Russia" || feature.get("name") == "Germany") {
+          styleOpen.setText(new Text({text: feature.get("name")}))
+          return  styleOpen;
         } else {
-          return new Style({
-            stroke: new Stroke({
-              color: "#3399CC",
-              width: 1.25,
-            }),
-          });
+          return styleClose;
         }
       },
     }),
